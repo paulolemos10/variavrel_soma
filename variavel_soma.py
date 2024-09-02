a7b458573eeaab1e4cb95434d7c9ec6562c3c495
Paulo Lemos <p.lemos10@outlook.com>
2024-09-02 17:02:37 -0300

    Add files via upload

---
+++ variavel_soma.py
@@ -0,0 +1,15 @@
+# Inicialização das variáveis
+indice = 12
+soma = 0
+k = 1
+
+# Loop que executa enquanto k for menor que indice
+while k < indice:
+    k = k + 1  # Incrementa k
+    soma = soma + k  # Adiciona k à soma
+
+# Imprime o valor final de soma
+print(soma)
+
+
+
